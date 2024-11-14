8c803f424501625e73e0beeb720485cf73e122dd
huca06001 <huca06001@E-PW05C37R>
2024-11-14 12:33:15 +0100

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder <16)
+            {
+                Console.WriteLine("Du är för ung");
+                
+            }
+            else if (ålder >19)
+            {
+                Console.WriteLine("Du är för gammal");
+            }
+            else
+            {
+                Console.WriteLine("Du är i rätt ålder");
+            }
+        }
+    }
+}
